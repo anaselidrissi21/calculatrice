afe3eff13f9bf89c54fe6e8771f28bbbda8776a2
Anas <anas.elidrissi@ammareal.fr>
2024-08-03 15:49:50 +0200

    feat: added calculator functionalityé

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Enter first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter an operator (+, -, *, /):");
+        char operator = scanner.next().charAt(0);
+
+        System.out.println("Enter second number:");
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = calculator.add(num1, num2);
+                break;
+            case '-':
+                result = calculator.subtract(num1, num2);
+                break;
+            case '*':
+                result = calculator.multiply(num1, num2);
+                break;
+            case '/':
+                try {
+                    result = calculator.divide(num1, num2);
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Error! " + e.getMessage());
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error! Invalid operator.");
+                return;
         }
+
+        System.out.println("The result is: " + result);
     }
-}=
+}
