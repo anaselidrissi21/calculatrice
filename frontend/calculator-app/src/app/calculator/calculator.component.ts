ff294d4d381d17f5a17c10ebd36378ab1ab07472
Anas <anas.elidrissi@ammareal.fr>
2024-08-04 15:30:12 +0200

    feat: added front done with angular

--- frontend/calculator-app/src/app/calculator/calculator.component.ts
+++ frontend/calculator-app/src/app/calculator/calculator.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-calculator',
-  standalone: true,
-  imports: [],
-  templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+selector: 'app-calculator',
+templateUrl: './calculator.component.html',
+styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
+firstNumber: number = 0;
+secondNumber: number = 0;
+operator: string = '';
+result: string = '';
 
+calculate() {
+    let result: number;
+    switch (this.operator) {
+      case '+':
+        result = this.firstNumber + this.secondNumber;
+        break;
+      case '-':
+        result = this.firstNumber - this.secondNumber;
+        break;
+      case '*':
+        result = this.firstNumber * this.secondNumber;
+        break;
+      case '/':
+        if (this.secondNumber !== 0) {
+          result = this.firstNumber / this.secondNumber;
+        } else {
+          this.result = 'Error! Division by zero.';
+          return;
+        }
+        break;
+      default:
+        this.result = 'Error! Invalid operator.';
+        return;
+    }
+    this.result = 'Result: ' + result;
+  }
 }
+
